6174bbf0dd6fd13b9401bcd4ee749c97131a4aca
alejandro <alejandro.tarragona@davinci.edu.ar>
2023-06-24 20:00:49 +0200

    Mensaje del commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  images: {
+    domains: ['jsonkeeper.com', 'upload.wikimedia.org', 'links.papareact.com'],
+  },
+  env: {
+    mapbox_key: "pk.eyJ1IjoiYWxldGVyY2lhIiwiYSI6ImNsYXd6czJ6ODBreHEzbm83NGNyZHRsdzkifQ.kmHvlbpa4SYaRrbud2LOGA",
+  },
+};
